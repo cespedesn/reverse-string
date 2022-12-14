cfc23b5b6d5eb1b83c0bfdb60125380cebb77a3d
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-12-14 11:50:02 -0500

    commit tree 675c545c34491de14911e0a37d6e5b3174014c47

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let str = 'ih'
+  let output = ''
+    for( let i = str.length - 1; i >= 0; i--) {
+        output = output + str[i]
+  }
 }
 
 if (require.main === module) {
@@ -17,3 +22,12 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// use let since variable is changing. In this case, i
+// need to use str.length -1 to start at the end of the word
+// -1 because of 0 index
+// make i greater than or equal to 0 since we are moving in decending order
+// and want to reverse it without using reverse method
+
+
+
